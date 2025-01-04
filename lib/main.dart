ae3f974341d5b3f2d8b011631c3b1cb84d215e0c
Trysor-llachar <trysorllachar@gmail.com>
2025-01-04 22:15:31 +0700

    try

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,14 @@ import 'login/view/login.dart';
 void main()async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  runApp( LoginScreen());
+  runApp(const MyApp());
 }
+class MyApp extends StatelessWidget {const MyApp({super.key});
 
+@override
+Widget build(BuildContext context) {
+  return MaterialApp( // Hoặc WidgetsApp
+    home: LoginScreen(),
+  );
+}
+}
